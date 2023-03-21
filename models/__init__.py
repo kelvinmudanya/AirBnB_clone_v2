586aaa772f5257f303dfb754d3688b89a35d92a0
kelvinmudanya <32190945+kelvinmudanya@users.noreply.github.com>
2023-03-21 21:56:14 +0300

    Create __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-
+"""This module switch allowing to change storage type directly
+ by using an environment variable """
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
+from os import getenv
 from models.user import User
+from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from models.place import Place
 from models.review import Review
-from os import getenv
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
