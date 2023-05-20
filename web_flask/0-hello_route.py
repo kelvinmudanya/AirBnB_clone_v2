39d79c3ad4049363350c4338544fd51b85ad717d
kelvinmudanya <32190945+kelvinmudanya@users.noreply.github.com>
2023-05-20 13:04:32 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application"""
+
+
+"""utilizing Flask for Web app frame work"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def display_hello():
+@app.route("/airbnb-onepage/", strict_slashes = False)
+def home():
+    """method that defines '/' route for Flask web application"""
     return "Hello HBNB!"
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000)
