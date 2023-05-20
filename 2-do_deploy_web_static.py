2347bdcbb089e285fc5a1e1b38fba66dabbdc262
kelvinmudanya <32190945+kelvinmudanya@users.noreply.github.com>
2023-05-20 13:01:58 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
+# a Fabric script that generates a .tgz archive
+# from the contents of web_static
+import os.path
+from fabric.api import *
+from fabric.contrib import files
 
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+
+env.user = "ubuntu"
+env.hosts = ['104.196.182.248', '3.95.214.115']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """ Transfers archive_path to web servers above
+    """
+
+    if not os.path.isfile(archive_path):
         return False
+
+    basename = os.path.basename(archive_path)
+    root, ext = os.path.splitext(basename)
+    target = '/data/web_static/releases/{}'.format(root)
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}/".format(target))
+        run("sudo tar -xzf /tmp/{} -C {}/".format(basename, target))
+        run("sudo rm /tmp/{}".format(basename))
+        run("sudo mv {}/web_static/* {}/".format(target, target))
+        run("sudo rm -rf {}/web_static".format(target))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(target))
+        print('New version uploaded!')
     except:
         return False
+    else:
+        return True
